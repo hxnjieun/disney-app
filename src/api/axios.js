bcc50d68bec11d58bca2c6bda9bf503613748ff2
Jieun <152059491+hxnjieun@users.noreply.github.com>
2023-12-06 17:48:51 +0900

    Add files via upload

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance=axios.create({
     baseURL:"https://api.themoviedb.org/3/",
     params:{
+        // api_key:"01d221ae714f2074940304fcc52ec991",
         api_key:"8340b6acccb9083ffed358cfa77e320c",
         language:"ko-KR",
     }
